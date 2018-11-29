17826e8524569c633a49d84fc536d429efefc90a
nazufel <ryanalexander.ross@gmail.com>
2018-11-29 16:19:54 -0500

    working on data composition for templates. did 01.

---
+++ notes/section-2-templates/011-composition/01/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+type person struct {
+	Name string
+	Age  int
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	p1 := person{
+		Name: "James Bond",
+		Age:  42,
+	}
+	err := tpl.Execute(os.Stdout, p1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
