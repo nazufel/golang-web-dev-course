8a2fd4abb82e5088a29e6ea500e66c2e91df0d22
nazufel <ryanalexander.ross@gmail.com>
2018-12-06 08:29:34 -0500

    favicon working

--- notes/section-6-serving-files/03-log-error/02-favicon-not-found/main.go
+++ notes/section-6-serving-files/03-log-error/02-favicon-not-found/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
 
-	// routes
-	http.HandleFunc("/", dog)
+	// route for root
+	http.HandleFunc("/", index)
+	// handling favicon requests since I don't have one
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
-func dog(w http.ResponseWriter, r *http.Request) {
-	// code for dog func
+func index(w http.ResponseWriter, r *http.Request) {
+	// print the path requested in the terminal
+	fmt.Println(r.URL.Path)
+	// write back to the connection to check the temrminal
+	fmt.Fprintln(w, "go look at your terminal")
 }
