a780a2ca8eb41afc8b9e25868d710423693313b8
nazufel <ryanalexander.ross@gmail.com>
2018-12-06 10:04:36 -0500

    added func main to stop compile errors.

--- notes/section-7-deploy-appengine/01/main.go
+++ notes/section-7-deploy-appengine/01/main.go
@@ -9,3 +9,8 @@ import (
 func init() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
 }
+
+// comment out func main() for deployments to app engine. added it so that my editor will stop flagging compile errors.
+func main() {
+
+}
