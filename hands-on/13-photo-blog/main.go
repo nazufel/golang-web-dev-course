bdadafec6e33c6b000e0955c0d74533db185fa28
nazufel <ryanalexander.ross@gmail.com>
2018-12-16 23:34:38 -0500

    adding more data into the cookie

--- hands-on/13-photo-blog/main.go
+++ hands-on/13-photo-blog/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/satori/go.uuid"
 	"html/template"
 	"net/http"
+	"strings"
 )
 
 var tpl *template.Template
@@ -24,8 +25,14 @@ func index(w http.ResponseWriter, r *http.Request) {
 	// get the cookie by running the getCookie func
 	c := getGooke(w, r)
 
+	// check and set for image names amd append to cookie var
+	c = appendCookie(w,c)
+
+	// split out the strings into a slice of strings
+	xs := strings.Split(c.Value, "|")
+
 	// execute the templates after getting the cookie
-	tpl.ExecuteTemplate(w, "index.gohtml", c.Value)
+	tpl.ExecuteTemplate(w, "index.gohtml", xs)
 
 }
 
@@ -46,6 +53,33 @@ func getGooke(w http.ResponseWriter, r *http.Request) *http.Cookie {
 		// write the cookie to the client
 		http.SetCookie(w, c)
 	}
-	// return the cookie value out of the func
+	// return the cookie value out of the func and back into the calling index func
+	return c
+}
+
+func appendCookie(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	// set the values for images
+	i1 := "disnelyland.jpg"
+	i2 := "atbeach.jgp"
+	i3 := "hollywood.jpg"
+
+	// append the cookie with a string
+	s := c.Value
+
+	// Check if the cookie contains "disneyland.jpg" if not, append
+	if !strings.Contains(s, i1) {
+		s += "|" + i1
+	}
+	// Check if the cookie contains "atbeach.jpg" if not, append
+	if !strings.Contains(s, i2) {
+		s += "|" + i2
+	}
+	// Check if the cookie contains "hollywood.jpg" if not, append
+	if !strings.Contains(s, i3) {
+		s += "|" + i3
+	}
+	// assign appended s to cookie
+	c.Value = s
+	http.SetCookie(w,c)
 	return c
 }
