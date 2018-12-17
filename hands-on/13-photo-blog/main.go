b0a78ab8cf01aa79bbc5030db10078ad2aee8187
nazufel <ryanalexander.ross@gmail.com>
2018-12-17 00:04:55 -0500

    D

--- hands-on/13-photo-blog/main.go
+++ hands-on/13-photo-blog/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"github.com/satori/go.uuid"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -25,9 +30,54 @@ func index(w http.ResponseWriter, r *http.Request) {
 	// get the cookie by running the getCookie func
 	c := getGooke(w, r)
 
-	// check and set for image names amd append to cookie var
-	c = appendCookie(w,c)
 
+	// process the submission form
+	if r.Method == http.MethodPost {
+		// get multipart file, file header and err
+		mf, fh, err := r.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		// close the file when done
+		defer mf.Close()
+
+		// pull out the extention value since strings.Split, splits a stirng
+		// into a slice of strings. the [1] is the "second" value split on
+		// on the ".".
+		ext := strings.Split(fh.Filename, ".")[1]
+
+		// create sha for file
+		h := sha1.New()
+
+		// copy the file name to the hashed file name
+		io.Copy(h, mf)
+
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+
+		// get the working directory
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// join the path for creating a file
+		p := filepath.Join(wd, "public", "pics", fname)
+
+		// create the file
+		nf, err := os.Create(p)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// close the new file
+		defer nf.Close()
+
+		// copy contents of old file to new
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+
+		// add the file name to the user's cookie
+		c = appendCookie(w, c, fname)
+	}
 	// split out the strings into a slice of strings
 	xs := strings.Split(c.Value, "|")
 
@@ -57,27 +107,15 @@ func getGooke(w http.ResponseWriter, r *http.Request) *http.Cookie {
 	return c
 }
 
-func appendCookie(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
-	// set the values for images
-	i1 := "disnelyland.jpg"
-	i2 := "atbeach.jgp"
-	i3 := "hollywood.jpg"
-
+func appendCookie(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
 	// append the cookie with a string
 	s := c.Value
 
-	// Check if the cookie contains "disneyland.jpg" if not, append
-	if !strings.Contains(s, i1) {
-		s += "|" + i1
+	// Check if the cookie contains the image string if not, append
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
 	}
-	// Check if the cookie contains "atbeach.jpg" if not, append
-	if !strings.Contains(s, i2) {
-		s += "|" + i2
-	}
-	// Check if the cookie contains "hollywood.jpg" if not, append
-	if !strings.Contains(s, i3) {
-		s += "|" + i3
-	}
+
 	// assign appended s to cookie
 	c.Value = s
 	http.SetCookie(w,c)
